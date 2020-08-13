d2eb1259440f24605dfe1715c7d2959fbde14748
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-13 19:10:26 -0400

    add footer and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Store from './components/Store/Store';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Store />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <footer>
+    <p>&copy;2020 <a href="https://robotspacefish.dev/" target="_blank" rel="noopener noreferrer" title="Opens in new window">robotspacefish!</a></p>
+  </footer>
+);
+
+export default Footer;
