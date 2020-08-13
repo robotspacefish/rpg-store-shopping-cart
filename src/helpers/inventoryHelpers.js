4f19ac62a67b5866e098d0fcfb30a22edc9434ce
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-12 23:07:04 -0400

    helper to add inventory to firebase db

---
+++ src/helpers/inventoryHelpers.js
@@ -0,0 +1,26 @@
+const postItem = (item) => {
+  fetch('https://rpg-store-shopping-cart.firebaseio.com/inventory.json', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(item)
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+};
+
+export const addItemsToStore = () => {
+  const items = [
+    { name: "Sword", price: 100 },
+    { name: "Shield", price: 60 },
+    { name: "Power Ring", price: 250 },
+    { name: "Bow", price: 90 },
+    { name: "Arrow", price: 5 },
+    { name: "Bomb", price: 10 }
+  ];
+
+  items.forEach(item => postItem(item))
+};
