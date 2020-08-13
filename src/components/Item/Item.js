707122136619fb6aa87f089a09a51803e573102f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-13 00:09:48 -0400

    change Item containers to li with spans

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,15 +2,10 @@ import React from 'react';
 import './Item.css';
 
 const Item = props => (
-  <div className="Item">
-    <div className="Item__name">
-      {props.name}
-    </div>
-
-    <div className="Item__price">
-      {props.price}g
-    </div>
-  </div>
+  <li className="Item">
+    <span>{props.name}</span>
+    <span>{props.price}g</span>
+  </li>
 );
 
 export default Item;
