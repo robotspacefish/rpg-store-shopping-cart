39ec176d2edc638a41f2beb62a614eef1c82f3a6
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-13 19:20:53 -0400

    todo onMouseOver delete text

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -4,9 +4,13 @@ import React from 'react';
 // For now, use this to display item name and price x quantity
 // click to delete?
 
+const handleMouseOver = (e) => {
+  // TODO delete text
+};
+
 const CartItem = props => (
   <div className="CartItem">
-    <li className="Item" onClick={() => { }}>
+    <li className="Item" onMouseOver={handleMouseOver}
       onClick={() => props.deleteFromCart(props.name)}>
       <span>{props.name}</span>
       <span>{props.totalCost}g</span>
