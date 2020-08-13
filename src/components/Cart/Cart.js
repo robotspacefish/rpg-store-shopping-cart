7ae7e8825ebac600f963400ce2ee5ac5f8ec74ac
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-13 18:54:19 -0400

    display cart total

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import CartItem from '../CartItem/CartItem';
 
+const multiplier = (price, qty) => price * qty;
+
+const reducer = (accumulator, currentValue) => (
+  accumulator + multiplier(currentValue.price, currentValue.quantity)
+);
+
 const Cart = props => {
+
   const renderCartItems = () => (
     props.cart.map(item => (
-      <CartItem key={item} {...item} />
+      <CartItem key={item.name} name={item.name} totalCost={multiplier(item.price, item.quantity)} />
     ))
   );
+
+  const total = () => props.cart.reduce(reducer, 0);
 
   return (
     <div className="Cart">
       <h2>Cart</h2>
       {renderCartItems()}
+
+      <div>
+        <span>TOTAL:</span>
+        <span>{props.cart.length !== 0 ? total() : 0}g</span>
+      </div>
+
     </div>
   );
 };
