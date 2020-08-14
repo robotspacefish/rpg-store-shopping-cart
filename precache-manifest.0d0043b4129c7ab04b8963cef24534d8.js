601844c3e35f45c305817f07b2e46afed6c02893
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-14 15:07:06 -0400

    Updates

---
+++ precache-manifest.0d0043b4129c7ab04b8963cef24534d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e216d22e4b6eeb805abee9d1e7192fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95b98ae697ba32826801",
+    "url": "/static/css/main.5690e0ef.chunk.css"
+  },
+  {
+    "revision": "f7596e3a51ed5eb1c1fc",
+    "url": "/static/js/2.ac384760.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac384760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b98ae697ba32826801",
+    "url": "/static/js/main.f72312cf.chunk.js"
+  },
+  {
+    "revision": "1b2bda726d65beb5751c",
+    "url": "/static/js/runtime-main.d5a1297d.js"
+  }
+]);
