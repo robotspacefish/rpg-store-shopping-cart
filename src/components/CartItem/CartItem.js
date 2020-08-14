08a72d33804ccfc334621658dfc7d0a743ca60f0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-08-14 16:40:53 -0400

    add remove button when mousing over item in cart

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ItemText from '../ItemText/ItemText';
 import './CartItem.css';
 
-const handleMouseOver = (e) => {
-  // TODO delete text
+const CartItem = props => {
+  const [isMousedOver, setIsMousedOver] = useState(true);
+
+  const renderRemoveButton = () => {
+    return <button className="CartItem__removeButton" onClick={() => props.deleteFromCart(props.name)}>Remove</button>
+  }
+
+  return (
+    <li className="CartItem"
+      onMouseEnter={(e) => setIsMousedOver(true)}
+      onMouseLeave={(e) => setIsMousedOver(false)}
+    >
+
+      {isMousedOver && renderRemoveButton()}
+
+      <ItemText text={props.name} />
+      <ItemText text={`x${props.quantity}`} />
+      <ItemText text={`${props.totalCost}g`} />
+    </li >
+  );
 };
-
-const CartItem = props => (
-  <li className="CartItem" onMouseOver={handleMouseOver}
-    onClick={() => props.deleteFromCart(props.name)}>
-    <ItemText text={props.name} />
-    <ItemText text={`x${props.quantity}`} />
-    <ItemText text={`${props.totalCost}g`} />
-  </li>
-);
-
 export default CartItem;
