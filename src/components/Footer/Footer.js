12196b54133fcf93944abea50f941cc5fdf39841
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-21 16:10:34 -0500

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
   <footer>
-    <p>&copy;2020 <a href="https://robotspacefish.dev/" target="_blank" rel="noopener noreferrer" title="Opens in new window">robotspacefish!</a></p>
+    <p>Made by <a href="https://robotspacefish.dev/" target="_blank" rel="noopener noreferrer" title="Opens in new window">robotspacefish!</a></p>
   </footer>
 );
 
