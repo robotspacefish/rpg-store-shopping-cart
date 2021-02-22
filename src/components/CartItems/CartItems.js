6400448a3a3d75fa97bb217e84a6af26985df6c3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-21 19:13:29 -0500

    use MODAL constant for modalType

--- src/components/CartItems/CartItems.js
+++ src/components/CartItems/CartItems.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ItemList from '../ItemList/ItemList';
 import { total } from '../../helpers/cartHelpers';
+import { MODAL } from '../Store/Store';
 
 const CartItems = ({ items, dispatch }) => (
   <div className="Cart items-container">
     <h2>Cart</h2>
 
-    <ItemList type="Cart" items={items} dispatch={dispatch} isCart />
+    <ItemList
+      type="Cart"
+      items={items}
+      dispatch={dispatch}
+      isCart
+      modalType={MODAL.REMOVE}
+    />
 
     <div className="Cart__total">
       <span>TOTAL:</span>
--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ItemList from '../ItemList/ItemList';
 import { STORE_ITEMS } from '../../helpers/inventoryHelpers';
+import { MODAL } from '../Store/Store';
 
 const InventoryItems = ({ dispatch }) => {
   return (
     <div className="Inventory items-container">
       <h2>Inventory</h2>
 
-      <ItemList type="Inventory" items={STORE_ITEMS} dispatch={dispatch} />
+      <ItemList
+        type="Inventory"
+        items={STORE_ITEMS}
+        dispatch={dispatch}
+        modalType={MODAL.ADD}
+      />
     </div>
   )
 }
