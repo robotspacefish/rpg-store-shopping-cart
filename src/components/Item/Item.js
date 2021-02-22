c76ea377fbbb51403e033d7496bc21cc17788ef0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-21 19:13:54 -0500

    pass modalType

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Item.css';
 import { ACTIONS } from '../Store/Store';
 
-const Item = ({ item, className, dispatch, children }) => {
+const Item = ({ item, className, dispatch, modalType, children }) => {
   return (
     <li className={className}
       onClick={() => (
-        dispatch({ type: ACTIONS.SET, modalType: MODAL.ADD, item })
+        dispatch({ type: ACTIONS.SET, modalType, item })
       )}
     >
       {children}
